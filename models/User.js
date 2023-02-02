9eb95f8bd4a4ada7f6e7f00989471510b4335de9
Brynner03 <brynner@google.com>
2023-02-02 15:58:57 -0500

    Made routes folders

---
+++ models/User.js
@@ -0,0 +1,12 @@
+//import our db, Model, DataTypes
+const { db, DataTypes } = require('../db')
+
+//Creating a User child class from the Model parent class
+const User = db.define("user", {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+});
+
+//exports
+module.exports = { User }
