445832d404dcae88e0ab15327dc32d5c361ab611
Brynner03 <brynner@google.com>
2023-02-23 16:17:59 -0500

    Working on getting frontend to talk to backend

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Footer } from "./Footer";
+import apiURL from './api';
 
 export const App = () => {
+    async function fetchUsers(){
+		try {
+			const response = await fetch(`${apiURL}/users`);
+			const usersData = await response.json();
+			setUsers(usersData);
+            console.log(usersData)
+		} catch (err) {
+			console.log("Oh no an error! ", err)
+		}
+	}
+
+    useEffect(() => {
+		fetchUsers();
+	}, []);
+
     return (
         <main>
             <div>
-                <h1>Hello</h1>
+                <h1>Hello&#123;</h1>
             </div>
             <Footer/>
         </main>
